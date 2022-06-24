ddd407ec3ec5ae893a48e5ba9f23d56e40977586
ajaysake1992 <41822477+ajaysake1992@users.noreply.github.com>
2022-06-24 22:48:46 +0530

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
-import './App.scss';
-import Header from './components/header';
-import Footer from './components/footer/footer';
-import Main from './components/main/main';
+import "./App.scss";
+import Header from "./components/header";
+import Footer from "./components/footer/footer";
+import Main from "./components/main/main";
 
 function App() {
   return (
     <div className="App">
-      <Header name="Ajinath Sake" role="FE Developer"/>
-      <Main/>
-      <Footer/>
+      <Header name="Ajinath Sake" role="Frontend Architect at Deutsche bank" />
+      <Main />
+      <Footer />
     </div>
   );
 }
